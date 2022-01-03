c25effc2e4d52baa349d9ba3433af618f17dbf6b
Alexis Velázquez <70240583+AVelazquez97@users.noreply.github.com>
2022-01-02 23:07:34 -0300

    Se crea objeto calculadora y se le asignan sus funciones básicas.

--- calculadora.py
+++ calculadora.py
@@ -2,33 +2,45 @@
 
 import os
 
-#Recibe dos números e imprime en pantalla el resultado de sumarlos entre sí
-def suma():
-    n1, n2 = solicitarValores()
-    print(f"El resultado de la suma es: {n1 + n2}")
+#Calculadora con sus funciones básicas: suma, resta, multiplicación y división.
+class calculator(object):
+    def __init__(self):
+        pass
 
-#Recibe dos números e imprime en pantalla el resultado de restarle el 2do al 1ero
-def resta():
-    n1, n2 = solicitarValores()
-    print(f"El resultado de la resta es: {n1 - n2}")
+    #Recibe dos números e imprime en pantalla el resultado de sumarlos entre sí    
+    def suma():
+        n1, n2 = solicitarValores()
+        print(f"Resultado: {n1} + {n2} = {n1 + n2}")
+    
+    #Recibe dos números e imprime en pantalla el resultado de restarle el 2do al 1ero
+    def resta():
+        n1, n2 = solicitarValores()
+        print(f"Resultado: {n1} - {n2} = {n1 - n2}")
 
-#Recibe dos números e imprime en pantalla el resultado de multiplicarlos entre sí
-def multiplicacion():
-    n1, n2 = solicitarValores()
-    print(f"El resultado de la multiplicación es: {n1 * n2}")
+    #Recibe dos números e imprime en pantalla el resultado de multiplicarlos entre sí
+    def multiplicacion():
+        n1, n2 = solicitarValores()
+        print(f"Resultado: {n1} * {n2} = {n1 * n2}")
 
-#Recibe dos números e imprime en pantalla el resultado de dividir el 1ero entre el 2
-def division():
-    n1, n2 = solicitarValores()
-    print(f"El resultado de la división es: {n1 / n2}")
+    #Recibe dos números e imprime en pantalla el resultado de dividir el 1ero entre el 2
+    def division():
+        n1, n2 = solicitarValores()
+        print(f"Resultado: {n1} / {n2} = {n1 / n2}")
+
 
 #Imprime en pantalla una lista de opciones disponibles para el usuario
 def menu():
-    print("1 .-Suma")
-    print("2 .-Resta")
-    print("3 .-Multiplicación")
-    print("4 .-División")
-    print("5 .-Salir\n")
+    print("╔═════════╗")
+    print("║  Calc:  ║")
+    print("╚═════════╝")
+    print("  1.Suma")
+    print("  2.Resta")
+    print("  3.Multiplicación")
+    print("  4.División")
+    print("  5.Salir\n")
+
+    opcion = input("Ingrese una opción(1..5): ")
+    return opcion
 
 #Retorna True si la variable recibida es un float y en caso contrario retorna False
 def es_flotante(variable):
@@ -49,26 +61,26 @@ def solicitarValores():
         if es_flotante(n1) and es_flotante(n2):
             centinela = False
         else:
-            print("No has ingresado valores válidos. Intenta nuevamente.")
+            print("\nNo has ingresado valores válidos. Intenta nuevamente.")
             
+    print()
     return (float(n1), float(n2))
 
 def main():
     while True:
-        menu()
-        opcion = input("Ingrese una opción(1..5): ")
+        opcion = menu()
 
         if opcion == '1':
-            suma()
+            calculator.suma()
 
         elif opcion == '2':
-            resta()
+            calculator.resta()
 
         elif opcion == '3':
-            multiplicacion()
+            calculator.multiplicacion()
 
         elif opcion == '4':
-            division()
+            calculator.division()
 
         elif opcion == '5':
             print("¡Has decidido salir. Gracias por usar la calculadora!")
